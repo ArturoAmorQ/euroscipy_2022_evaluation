37ab0efa6f7865f5466514a2aecb8bd2b0642344
Arturo Amor <86408019+ArturoAmorQ@users.noreply.github.com>
2022-08-19 09:55:02 +0200

    Add python files
    Adds files corresponding to 2/3 tutorials: the evaluation tutorial and the tuto on ROC and PR curves.

---
+++ python_scripts/1_evaluation_tutorial.py
@@ -0,0 +1,236 @@
+# %%
+# Accounting for imbalance in evaluation metrics for classification 
+# =================================================================
+#
+# Suppose we have a population of subjects with features `X` that can hopefully
+# serve as indicators of a binary class `y` (known ground truth). Additionally,
+# suppose the class prevalence (the number of samples in the positive class
+# divided by the total number of samples) is very low.
+#
+# To fix ideas, let's use a medical analogy and think about diabetes. We only
+# use two features -age and blood sugar level-, to keep the example as simple as
+# possible. We use `make_classification` to simulate the distribution of the
+# disease and to ensure the data-generating process is always the same. We set
+# the `weights=[0.99, 0.01]` to obtain a prevalence of around 1% which,
+# according to the World Bank, is the case for the country with the lowest
+# diabetes prevalence in 2022 (Benin).
+# 
+# In practice, the ideas presented here can be applied in settings where the
+# data available to learn and evaluate a classifier has nearly balanced classes,
+# such as a case-control study, while the target application, i.e. the general
+# population, has very low prevalence.
+
+from sklearn.datasets import make_classification
+
+common_params = {
+    "n_samples": 10_000,
+    "n_features": 2,
+    "n_informative": 2,
+    "n_redundant": 0,
+    "n_classes": 2, # binary classification
+    "shift": [4, 6],
+    "scale": [10, 25],
+    "random_state": 0,
+}
+X, y = make_classification(**common_params, weights=[0.99, 0.01])
+prevalence = y.mean()
+print(f"Percentage of people carrying the disease: {100*prevalence:.2f}%")
+
+# %%
+# A simple model is trained to diagnose if a person is likely to have diabetes.
+# To estimate the generalization performance of such model, we do a train-test
+# split.
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+
+estimator = DecisionTreeClassifier(max_depth=2, random_state=0).fit(X_train, y_train)
+
+# %%
+# We now show the decision boundary learned by the estimator. Notice that we
+# only plot an stratified subset of the original data.
+
+import matplotlib.pyplot as plt
+from sklearn.inspection import DecisionBoundaryDisplay
+
+fig, ax = plt.subplots()
+disp = DecisionBoundaryDisplay.from_estimator(
+    estimator,
+    X_test,
+    response_method="predict",
+    alpha=0.5,
+    xlabel="age (years)",
+    ylabel="blood sugar level (mg/dL)",
+    ax=ax,
+)
+scatter = disp.ax_.scatter(X_test[:, 0], X_test[:, 1], c=y_test, edgecolor="k")
+disp.ax_.set_title(f"Diabetes test with prevalence = {y.mean():.2f}")
+_ = disp.ax_.legend(*scatter.legend_elements())
+
+# %%
+# The most widely used summary metric is arguably accuracy. Its main advantage
+# is a natural interpretation: the proportion of correctly classified samples.
+# However, it is misleading when the data is imbalanced. Our model performs
+# better than a trivial majority classifier in only 3 of the samples.
+
+from sklearn import metrics
+
+y_pred = estimator.predict(X_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print(f"Accuracy on the test set: {accuracy:.2f}")
+
+# %%
+# Some of the other metrics are better at describing the flaws of our model:
+
+sensitivity = metrics.recall_score(y_test, y_pred)
+specificity = metrics.recall_score(y_test, y_pred, pos_label=0)
+balanced_acc = metrics.balanced_accuracy_score(y_test, y_pred)
+matthews = metrics.matthews_corrcoef(y_test, y_pred)
+PPV = metrics.precision_score(y_test, y_pred)
+
+print(f"Sensitivity on the test set: {sensitivity:.2f}")
+print(f"Specificity on the test set: {specificity:.2f}")
+print(f"Balanced accuracy on the test set: {balanced_acc:.2f}")
+print(f"Matthews correlation coeff on the test set: {matthews:.2f}")
+print()
+print(f"Probability to have the disease given a positive test: {100*PPV:.2f}%")
+
+# %%
+# Our classifier is not informative enough on the general population. The PPV
+# and NPV give the information of interest: P(D+ | T+) and P(D− | T−). However,
+# they are not intrinsic to the medical test (in other words the trained ML
+# model) but also depend on the prevalence and thus on the target population.
+#
+# The class likelihood ratios (LR±) depend only on sensitivity and specificity
+# of the classifier, and not on the prevalence of the study population. For the
+# moment it suffice to recall that LR± is defined as
+#
+#     LR± = P(D± | T+) / P(D± | T−)
+
+pos_LR, neg_LR = metrics.class_likelihood_ratios(y_test, y_pred)
+print(f"LR+ on the test set: {pos_LR:.3f}") # higher is better
+print(f"LR- on the test set: {neg_LR:.3f}") #  lower is better
+
+# %%
+# Extrapolating between populations
+# ---------------------------------
+#
+# The prevalence can be variable (for instance the prevalence of an infectious
+# disease will be variable across time) and a given classifier may be intended
+# to be applied in various situations.
+#
+# According to the World Bank, the diabetes prevalence in the French Polynesia
+# in 2022 is above 25%. Let's now evaluate our previously trained model on a
+# **different population** with such prevalence and **the same data-generating
+# process**.
+
+X, y = make_classification(**common_params, weights=[0.75, 0.25])
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+
+fig, ax = plt.subplots()
+disp = DecisionBoundaryDisplay.from_estimator(
+    estimator,
+    X_test,
+    response_method="predict",
+    alpha=0.5,
+    xlabel="age (years)",
+    ylabel="blood sugar level (mg/dL)",
+    ax=ax,
+)
+scatter = disp.ax_.scatter(X_test[:, 0], X_test[:, 1], c=y_test, edgecolor="k")
+disp.ax_.set_title(f"Diabetes test with prevalence = {y.mean():.2f}")
+_ = disp.ax_.legend(*scatter.legend_elements())
+
+# %%
+# We compute the same metrics using a test set with the new prevalence:
+
+y_pred = estimator.predict(X_test)
+prevalence = y.mean()
+accuracy = metrics.accuracy_score(y_test, y_pred)
+sensitivity = metrics.recall_score(y_test, y_pred)
+specificity = metrics.recall_score(y_test, y_pred, pos_label=0)
+balanced_acc = metrics.balanced_accuracy_score(y_test, y_pred)
+matthews = metrics.matthews_corrcoef(y_test, y_pred)
+PPV = metrics.precision_score(y_test, y_pred)
+
+print(f"Accuracy on the test set: {accuracy:.2f}")
+print(f"Sensitivity on the test set: {sensitivity:.2f}")
+print(f"Specificity on the test set: {specificity:.2f}")
+print(f"Balanced accuracy on the test set: {balanced_acc:.2f}")
+print(f"Matthews correlation coeff on the test set: {matthews:.2f}")
+print()
+print(f"Probability to have the disease given a positive test: {100*PPV:.2f}%")
+
+# %%
+# The same model seems to perform better on this new dataset. Notice in
+# particular that the probability to have the disease given a positive test
+# increased. The same blood sugar test is less predictive in Benin than in
+# the French Polynesia!
+#
+# If we really want to score the test and not the dataset, we need a metric that
+# does not depend on the prevalence of the study population.
+
+pos_LR, neg_LR = metrics.class_likelihood_ratios(y_test, y_pred)
+
+print(f"LR+ on the test set: {pos_LR:.3f}")
+print(f"LR- on the test set: {neg_LR:.3f}")
+
+# %%
+# Despite some variations due to residual dataset dependence, the class
+# likelihood ratios are mathematically invariant with respect to prevalence. See
+# [this example from the User
+# Guide](https://scikit-learn.org/dev/auto_examples/model_selection/plot_likelihood_ratios.html#invariance-with-respect-to-prevalence)
+# for a demo regarding such property.
+
+# %%
+# Pre-test vs. post-test odds
+# ---------------------------
+#
+# Both class likelihood ratios are interpretable in terms of odds:
+#
+#     post-test odds = Likelihood ratio * pre-test odds
+#
+# The interpretation of LR+ in this case reads:
+
+print("The post-test odds that the condition is truly present given a positive "
+     f"test result are: {pos_LR:.3f} times larger than the pre-test odds.")
+
+# %% 
+# We found that diagnosis tool is useful: the post-test odds are larger than the
+# pre-test odds. We now choose the pre-test probability to be the prevalence of
+# the disease in the held-out testing set.
+
+pretest_odds = y_test.mean() / (1 - y_test.mean())
+posttest_odds = pretest_odds * pos_LR
+
+print(f"Observed pre-test odds: {pretest_odds:.3f}")
+print(f"Estimated post-test odds using LR+: {posttest_odds:.3f}")
+
+# %%
+# The post-test probability is the probability of an individual to truly have
+# the condition given a positive test result, i.e. the number of true positives
+# divided by the total number of samples. In real life applications this is
+# unknown.
+
+posttest_prob = posttest_odds / (1 + posttest_odds)
+
+print(f"Estimated post-test probability using LR+: {posttest_prob:.3f}")
+
+# %% 
+# We can verify that if we had had access to the true labels, we would have
+# obatined the same probabilities:
+
+posttest_prob = y_test[y_pred == 1].mean()
+
+print(f"Observed post-test probability: {posttest_prob:.3f}")
+
+# %%
+# Conclusion: If a Benin salesperson was to sell the model to the French Polynesia
+# by showing them the 59.84% probability to have the disease given a positive test,
+# the French Polynesia would have never bought it, even though it would be quite
+# predictive for their own population. The right thing to report are the LR±.
+#
+# Can you imagine what would happen if the model is trained on nearly balanced classes
+# and then extrapolated to other scenarios?
---
+++ python_scripts/2_roc_pr_curves_tutorial.py
@@ -0,0 +1,273 @@
+# %%
+# Evaluation of different probability thresholds
+# ----------------------------------------------
+#
+# All statistics that we presented up to now rely on `.predict` which outputs
+# the most likely label. We haven’t made use of the probability associated with
+# this prediction, which gives the confidence of the classifier in this
+# prediction. By default, the prediction of a classifier corresponds to a
+# threshold of 0.5 probability in a binary classification problem. Let's build a
+# toy dataset to illustrate this.
+
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+common_params = {
+    "n_samples": 10_000,
+    "n_features": 2,
+    "n_informative": 2,
+    "n_redundant": 0,
+    "n_classes": 2,  # binary classification
+    "random_state": 0,
+}
+X, y = datasets.make_classification(**common_params, weights=[0.6, 0.4])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, random_state=0, test_size=0.02
+)
+
+# %%
+# We can quickly check the predicted probabilities to belong to either class
+# using a `LogisticRegression`. To ease the visualization we select a subset
+# of `n_plot` samples.
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+
+n_plot = 10
+classifier = make_pipeline(StandardScaler(), LogisticRegression())
+classifier.fit(X_train, y_train)
+
+proba_predicted = pd.DataFrame(
+    100 * classifier.predict_proba(X_test), columns=classifier.classes_
+).round(decimals=1)
+proba_predicted[:n_plot]
+
+# %%
+# Probabilites sum to 100%. In the binary case it suffices to retain the
+# probability of belonging to the positive class, here shown as an annotation in
+# the `DecisionBoundaryDisplay`. Notice that setting
+# `response_method="predict_proba"` shows the level curves of the 2D sigmoid
+# (logistic curve).
+
+import matplotlib.pyplot as plt
+from sklearn.inspection import DecisionBoundaryDisplay
+
+fig, ax = plt.subplots()
+disp = DecisionBoundaryDisplay.from_estimator(
+    classifier,
+    X_test,
+    response_method="predict_proba",
+    alpha=0.5,
+    ax=ax,
+)
+scatter = disp.ax_.scatter(
+    X_test[:n_plot, 0], X_test[:n_plot, 1], c=y_test[:n_plot], edgecolor="k"
+)
+disp.ax_.set_title("This is a plot title")
+disp.ax_.legend(*scatter.legend_elements())
+for i, proba in enumerate(proba_predicted[:n_plot][1]):
+    disp.ax_.annotate(proba, (X_test[i, 0], X_test[i, 1]), fontsize="large")
+
+# %%
+# The default decision threshold (0.5) might not be the best threshold that
+# leads to optimal generalization performance of our classifier. One can vary
+# the decision threshold, and therefore the underlying prediction, and compute
+# the same evaluation metrics presented earlier. Usually, the recall and
+# precision are computed and plotted on a graph. Each point on the graph
+# corresponds to a specific decision threshold. Let’s start by computing the
+# precision-recall curve.
+
+from sklearn.metrics import PrecisionRecallDisplay
+from sklearn.dummy import DummyClassifier
+
+dummy_classifier = DummyClassifier(strategy="most_frequent")
+dummy_classifier.fit(X_train, y_train)
+
+disp = PrecisionRecallDisplay.from_estimator(
+    classifier, X_test, y_test, name="LogisticRegression", marker="+"
+)
+disp = PrecisionRecallDisplay.from_estimator(
+    dummy_classifier,
+    X_test,
+    y_test,
+    name="chance level",
+    color="tab:orange",
+    linestyle="--",
+    ax=disp.ax_,
+)
+
+plt.legend(loc="lower left")
+_ = disp.ax_.set_title("Precision-recall curve")
+
+# %%
+# Notice that the `DummyClassifier` is a way to define chance level, and
+# its average precision coincides with the prevalence of the positive class:
+
+print(f"Prevalence of the positive class: {y.mean():.2f}")
+
+# %%
+# On this curve, each blue cross corresponds to a level of probability which we
+# used as a decision threshold. We can see that, by varying this decision
+# threshold, we get different precision vs. recall values.
+#
+# A perfect classifier would have a precision of 1 for all recall values. A
+# metric characterizing the curve is linked to the area under the curve (AUC)
+# and is named average precision (AP). With an ideal classifier, the average
+# precision would be 1.
+#
+# The precision and recall metric focuses on the positive class. However, one
+# might be interested in the compromise between accurately discriminating the
+# positive class and accurately discriminating the negative classes. The
+# statistics used for this are sensitivity and specificity. On the one hand,
+# sensitivity is just another name for recall. On the other hand specificity
+# measures the proportion of correctly classified samples in the negative class
+# defined as: TN / (TN + FP). Similar to the precision-recall curve, sensitivity
+# and specificity are generally plotted as a curve called the Receiver Operating
+# Characteristic (ROC) curve. Below is such a curve:
+
+from sklearn.metrics import RocCurveDisplay
+
+
+disp = RocCurveDisplay.from_estimator(classifier, X_test, y_test, name="LogisticRegression", marker="+")
+disp = RocCurveDisplay.from_estimator(
+    dummy_classifier, X_test, y_test, name="chance level", color="tab:orange", linestyle="--", ax=disp.ax_
+)
+plt.legend()
+_ = disp.ax_.set_title("ROC AUC curve for the ")
+
+# %%
+# This curve was built using the same principle as the precision-recall curve:
+# we vary the probability threshold for determining "hard" prediction and
+# compute the metrics. As with the precision-recall curve, we can compute the
+# area under the ROC (ROC-AUC) to characterize the generalization performance of
+# our classifier. However, it is important to observe that the lower bound of
+# the ROC-AUC is 0.5. Indeed, we show the generalization performance of a dummy
+# classifier (the orange dashed line) to show that even the worst generalization
+# performance obtained will be above this line.
+#
+# Let's do something more realistic: We load the [Forest covertypes
+# dataset](https://scikit-learn.org/stable/datasets/real_world.html#covtype-dataset).
+# The samples in this dataset correspond to 30×30m patches of forest in the US,
+# collected for the task of predicting each patch’s cover type, i.e. the
+# dominant species of tree. There are seven covertypes, making this a multiclass
+# classification problem, but in this case we will only retain two of such
+# classes.
+
+X, y = datasets.fetch_covtype(return_X_y=True, as_frame=True)
+mask = np.isin(y, [1, 2])  # Select two classes
+X = X[mask]
+y = y[mask]
+
+# Downsample, to see sampling effects in curves
+X_reserve, X, y_reserve, y = train_test_split(
+    X, y, stratify=y, random_state=0, test_size=10_000
+)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, random_state=0, test_size=0.02
+)
+
+print(
+    f"prevalence of the majority class: {max(y.value_counts())/y.value_counts().sum():.2f}"
+)
+
+# %%
+# In this case we also explore some other models to give a visual intuition of what
+# is a good PR-AUC and ROC-AUC.
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+
+classifiers = {
+    "Excellent": RandomForestClassifier(n_jobs=-1, random_state=1),
+    "Good": make_pipeline(StandardScaler(), LogisticRegression()),
+    "Poor": GaussianNB(var_smoothing=0.3),
+    "Chance": DummyClassifier(strategy="most_frequent"),
+}
+
+for name, clf in classifiers.items():
+    clf.fit(X_train, y_train)
+
+
+# %%
+# The precision recall curve can be plotted using the `.predict_proba` for linear
+# models or the `.decision_function` in other cases.
+
+from sklearn.metrics import precision_recall_curve, average_precision_score
+
+fig = plt.figure()
+ax = plt.axes([0.18, 0.15, 0.78, 0.78])
+
+for name, clf in classifiers.items():
+    if hasattr(clf, "predict_proba"):
+        y_score = clf.predict_proba(X_test)[:, 1]
+    else:
+        y_score = clf.decision_function(X_test)
+    prec, recall, _ = precision_recall_curve(y_test, y_score, pos_label=clf.classes_[1])
+    auc = average_precision_score(y_test, y_score, pos_label=clf.classes_[1])
+    if name == "Chance":
+        # Use a much bigger test set: we do not want to illustrate
+        # sampling noise
+        y_score = clf.predict_proba(X_reserve)[:, 1]
+        y_score += 0.25 * np.random.random(size=y_score.shape)
+        prec, recall, _ = precision_recall_curve(
+            y_reserve, y_score, pos_label=clf.classes_[1]
+        )
+        auc = average_precision_score(y_reserve, y_score, pos_label=clf.classes_[1])
+    plt.plot(recall, prec, label=f"{name}, AUC={auc:.2f}")
+
+plt.xlabel("Recall                  ")
+plt.text(0.56, 0.0485, "= TPR or sensitivity", transform=fig.transFigure, size=7)
+plt.ylabel("Precision         ")
+plt.text(0.1, 0.6, "= PPV", transform=fig.transFigure, size=7, rotation="vertical")
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.title("Precision-recall curves for Forest Covertypes dataset")
+plt.legend(loc="best")
+plt.show()
+
+# %%
+print(f"Notice that the class imbalance {np.mean(y_reserve - 1):.4f}\n"
+      f"equals the chance AUC {auc:.4f}")
+
+# %%
+# We can evaluate the ROC curves of the same set of models using the Forest
+# Covertypes dataset
+
+from sklearn.metrics import roc_curve, roc_auc_score
+
+fig = plt.figure()
+ax = plt.axes([0.18, 0.15, 0.78, 0.78])
+
+for name, clf in classifiers.items():
+    if hasattr(clf, "predict_proba"):
+        y_score = clf.predict_proba(X_test)[:, 1]
+    else:
+        y_score = clf.decision_function(X_test)
+    fpr, tpr, _ = roc_curve(y_test, y_score, pos_label=clf.classes_[1])
+    auc = roc_auc_score(y_test, y_score)
+    if name == "Chance":
+        fpr = [0, 1]
+        tpr = [0, 1]
+        auc = 0.5
+    plt.plot(fpr, tpr, label=f"{name}, AUC={auc:.2f}")
+
+
+plt.xlabel("False positive rate")
+plt.ylabel("True positive rate                           ")
+plt.text(
+    0.098,
+    0.575,
+    "= sensitivity or recall",
+    transform=fig.transFigure,
+    size=7,
+    rotation="vertical",
+)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.title("ROC curves for Forest Covertypes dataset")
+plt.legend(loc="best")
+plt.show()
